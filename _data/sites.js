13e5f6bb553ccfb538578d794d2ae62f7d0a4b66
Dan Sullivan <dan@symbiosgroup.co.uk>
2020-07-02 10:06:01 +0100

    Split clearwaste and sh

--- _data/sites.js
+++ _data/sites.js
@@ -1,12 +1,21 @@
 module.exports = {
-	symbios: {
-		description: "Symbios web sites",
+	sh: {
+		description: "SH web sites",
 		options: {
 			frequency: 60 * 2, // 2 hours
 			freshChrome: "site",
 		},
 		urls: [
 			"https://www.swingingheaven.co.uk",
+		]
+	},
+	clearwaste: {
+		description: "ClearWaste web sites",
+		options: {
+			frequency: 60 * 2, // 2 hours
+			freshChrome: "site",
+		},
+		urls: [
 			"https://www.clearwaste.com",
 		]
 	},
